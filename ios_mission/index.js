20b0c699c55a5555ecc275cb6b3479e9c3bd850f
YenChunchen <rayTW0313@gmail.com>
2017-02-11 15:07:00 +0800

    Update index.js
    update CamelCase and fixed date

--- ios_mission/index.js
+++ ios_mission/index.js
@@ -3,57 +3,60 @@ var router=express.Router();
 var url=require('url');
 var today=new Date();
 var time={
-  year:today.getFullYear().toString(),
-  month:today.getMonth().toString(),
-  day:today.getDay().toString(),
-  hour:parseInt(today.getHours()),
-  min:parseFloat(today.getMinutes())
+  year:"2017",
+  month:"2",
+  day:"28",
+  hour:parseInt("12"),
+  min:parseFloat("15.5")
 };
 var weather;
+
+/*   mission1   */
 router.get('/m1',function(req,res){
   var name=req.query.name;
-  if ((name===undefined)||(name==='')) res.status(500).json({fail:'請輸入name' });
+  if ((name===undefined)||(name==='')) res.status(400).json({fail:'請輸入name' });
   var message='welcome '+name;
   res.json({welcome:message,time:time});
 });
-//-----------------------------------------------------------------------
+
+/*   mission2   */
 router.get('/m2',function(req,res){
   var weatherinput=req.headers.weatherinput;
   var name=req.headers.name;
-  checkfield(name,weatherinput,res);
+  CheckField(name,weatherinput,res);
   var message='welcome '+name;
-  chooseweather(weatherinput,req,res);
+  ChooseWeather(weatherinput,req,res);
   res.json({welcome:message,time:time,weather:weather});
 });
-//-----------------------------------------------------------------------
+
+/*   mission3   */
 router.post('/m3',function(req,res){
   var temp=req.body;
   var name=temp.name;
   var weatherinput=temp.weatherinput;
-  checkfield(name,weatherinput,res);
+  CheckField(name,weatherinput,res);
   var message='welcome '+name;
-  chooseweather(weatherinput,req,res);
+  ChooseWeather(weatherinput,req,res);
   res.json({welcome:message,time:time,weather:weather});
 });
-//-----------------------------------------------------------------------------
+
+/*   mission4   */
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
 router.post('/m4',upload.single(),function(req,res){
   var temp=req.body;
   var name=temp.name;
   var weatherinput=temp.weatherinput;
-  checkfield(name,weatherinput,res);
+  CheckField(name,weatherinput,res);
   var message='welcome '+name;
-  chooseweather(weatherinput,req,res);
+  ChooseWeather(weatherinput,req,res);
   res.json({welcome:message,time:time,weather:weather});
 });
 module.exports=router;
 
 
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-//------------------------------------------------------------------------------
-function chooseweather(weatherinput,req,res){
+/*依使用者輸入天氣名稱選擇不同圖片回應*/
+function ChooseWeather(weatherinput,req,res){
   switch (weatherinput){
     case 'sunny':{
       weather='https://'+req.hostname+'/uploads/sunny.jpg';
@@ -78,8 +81,8 @@ function chooseweather(weatherinput,req,res){
     }
   }
 }
-
-function checkfield(name,weatherinput,res){
-  if ((name===undefined)||(name==='')) res.status(500).json({fail:'請輸入name' });
-  if ((weatherinput===undefined)||(weatherinput==='')) res.status(500).json({fail:'請輸入正確天氣' });
+/*檢查m2,m3,m4使用者輸入欄位是否正確   */
+function CheckField(name,weatherinput,res){
+  if ((name===undefined)||(name==='')) res.status(400).json({fail:'請輸入name' });
+  if ((weatherinput===undefined)||(weatherinput==='')) res.status(400).json({fail:'請輸入正確天氣' });
 }
