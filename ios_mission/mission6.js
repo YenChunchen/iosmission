d58a34b5bce24af4ccf37aa1415033158e2e96f4
YenChunchen <rayTW0313@gmail.com>
2017-03-01 13:25:11 +0800

    update mission6 err

--- ios_mission/mission6.js
+++ ios_mission/mission6.js
@@ -3,12 +3,7 @@ var router = express.Router();
 var upload=require('./form_data_upload_multer');
 var fs=require('fs');
 /*新建會員*/
-router.get('/',function(err,req,res) {
-  if(err){
-    var fail={message:'sorry the system is broke'};
-    res.json({fail:fail});
-    return;
-  }
+router.get('/',function(req,res) {
   var bigPic='https://'+req.hostname+'/uploads/10M.jpg';
   var success={bigPic:bigPic};
   res.json({success:success});
